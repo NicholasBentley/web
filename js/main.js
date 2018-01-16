a52079b44511e797618e6e1689fa7e20d6ad0e1e
Nicholas Bentley <njbentley1@gmail.com>
2018-01-15 23:06:36 -0500

    Revert "update"
    This reverts commit 73a381ec008e9c58f13a1e88da2bd71ae45e2e25.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,3 @@ jQuery('.menulink').click(function(){
   jQuery('.menulink').removeClass('active');
   jQuery(this).addClass('active');
 });
-$('#contact').click(function() {
-    $('#contact').addClass('active');
-});
-function linkActive()
-{
-    document.getElementById('link').setAttribute("class", "menulink active");
-}
