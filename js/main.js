73a381ec008e9c58f13a1e88da2bd71ae45e2e25
Nicholas Bentley <njbentley1@gmail.com>
2018-01-15 22:57:13 -0500

    update

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ jQuery('.menulink').click(function(){
   jQuery('.menulink').removeClass('active');
   jQuery(this).addClass('active');
 });
+$('#contact').click(function() {
+    $('#contact').addClass('active');
+});
+function linkActive()
+{
+    document.getElementById('link').setAttribute("class", "menulink active");
+}
