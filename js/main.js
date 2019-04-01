e69fc0a687e696aad792221a22afd050e8421ed1
Nick Bentley <njbentley1@gmail.com>
2019-04-01 14:15:42 -0400

    finally add the menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-jQuery('.menulink').click(function(){
-  jQuery('.menulink').removeClass('active');
-  jQuery(this).addClass('active');
+$(function() {
+  var curPage="descr1";
+  $("#menu a").click(function() {
+      if (curPage.length) { 
+          $("#"+curPage).hide();
+      }
+      curPage=$(this).data("descr");
+      $("#"+curPage).show();
+  });
+});
+
+$(function () {
+  var menuLink="link1";
+  $("#menu a").click(function() {
+    if (menuLink.length) {
+      $("#"+menuLink).removeClass("active");
+    }
+    menuLink=$(this).data("link");
+    $("#"+menuLink).addClass("active");
+  });
 });
