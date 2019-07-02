7e0381af05abfeaa2881e07dc5062ca9b0f942a9
Nicholas Bentley <nbentley@umd.edu>
2019-07-02 13:06:23 -0400

    update appointments config

--- js/main.js
+++ js/main.js
@@ -20,10 +20,21 @@ $(function () {
   });
 });
 
-$.urlParam = function(name){
-  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-  if (results==null) {
-     return null;
-  }
-  return decodeURI(results[1]) || 0;
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  return vars;
 }
+function getUrlParam(parameter, defaultvalue){
+  var urlparameter = defaultvalue;
+  if(window.location.href.indexOf(parameter) > -1){
+      urlparameter = getUrlVars()[parameter];
+      }
+  return urlparameter;
+}
+
+var calInviteeName = getUrlParam('invitee_first_name','for scheduling');
+var calInviteeEmail = getUrlParam('invitee_email','the email address you provided');
+var calInviteeDate = decodeURIComponent(getUrlParam('event_start_time'));
