e13b1de32bf11ae4dc406122b8f51b663781bc5f
Nicholas Bentley <nbentley@umd.edu>
2019-07-02 11:03:28 -0400

    add appointments

--- js/main.js
+++ js/main.js
@@ -19,3 +19,11 @@ $(function () {
     $("#"+menuLink).addClass("active");
   });
 });
+
+$.urlParam = function(name){
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results==null) {
+     return null;
+  }
+  return decodeURI(results[1]) || 0;
+}
